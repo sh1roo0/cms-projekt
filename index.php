43df39ead37e7a9f500b0957884c4eefc82f12fc
Kamil6747 <kamlbaron3523@gmail.com>
2026-03-11 15:11:31 +0100

    visual

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="container">
+      
+        
+    <aside>
+        <a href="">Artykuly</a>
+        <a href="">Nawigacja</a>
+        <a href="">Stopka</a>
+        <a href="">Ustawienia</a>
+
+        
+
+    </aside>
+
+    <div class="header">
+        <a href="">cos</a>
+        <a href="">cos</a>
+        <a href="">cos</a>
+        <a href="">cos</a>
+
+
+        <div class="login">
+             <a href="zaloguj.php" id="login">zaloguj sie</a>
+        </div>
+       
+    </div>
+
+    <main>
+
+        <div class="article-wrapper">
+
+        <article>
+            
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+        <article>
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+       <article>
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+        <article>
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+        <article>
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+        <article>
+            <img src="https://picsum.photos/200/140" alt="">
+
+            <h1>fajny artykul1</h1>
+        </article>
+
+        </div>
+
+        
+
+    </main>
+
+
+
+    <div class="footer">
+
+        <h1>cos</h1>
+
+    </div>
+    </section>
+</body>
+</html>
